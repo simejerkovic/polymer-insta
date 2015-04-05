6273575d015939dd1dcf5461144ab77f28946fd5
Šime Jerković <sime@sistemi.hr>
2015-04-05 22:48:00 +0200

    init

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp        = require('gulp');
+var deploy      = require('gulp-gh-pages');
+
+
+/*******************************************************************************
+    COPY TO DIST
+*******************************************************************************/
+var options = {
+	cacheDir: '../foo' 
+};
+
+var filesToMove = [
+				'./**/*.*',
+				'!./node_modules/**/'
+        //'./icons/**/*.*',
+        //'./src/page_action/**/*.*',
+        //'./manifest.json'
+    ];
+// TODO rewrite for GULP v4
+gulp.task('copyto:dist', function () {
+  // the base option sets the relative root for the set of files,
+  // preserving the folder structure
+  gulp.src(filesToMove, {
+      base: './'
+    })
+    .pipe(gulp.dest('./dist'));
+});
+
+/**
+ * Push build to gh-pages
+ */
+gulp.task('deploy', function () {
+  return gulp.src(['./dist/**/*', '!./node_modules/**/'])
+    .pipe(deploy(options))
+});
